77bf7b243df465a88a556a5993388455e70316e3
murat <seras.murat@gmil.com>
2018-03-01 23:20:26 +0300

    Initial Commit

---
+++ LayoutApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LayoutApp
+//
+//  Created by MURAT ŞERAS on 1.03.2018.
+//  Copyright © 2018 MURAT ŞERAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
